f08858abb75e6d9d080955c9018939f3a6683476
Abhishek <abhishekkumar95084039@gmail.com>
2024-09-25 17:19:14 +0530

    first commit

---
+++ string(twinkle twinkle).py
@@ -0,0 +1,1 @@
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \nLike a diamond in the sky. \n\tTwinkle, twinkle, little star, \n\t\tHow I wonder what you are")
